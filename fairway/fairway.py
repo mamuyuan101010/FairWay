7839406c42bd38e25c06d7d17999d6343fefdf8e
mamuyuan101010 <114116666+mamuyuan101010@users.noreply.github.com>
2022-12-12 10:37:46 -0500

    commit

--- fairway/fairway.py
+++ fairway/fairway.py
@@ -1,13 +1,27 @@
-###
-# Main application interface
-###
-
 # import the create app function 
 # that lives in src/__init__.py
+# import the Flask framework
+from flask import Flask, jsonify
+from flask import request
+from flaskext.mysql import MySQL
 from src import create_app
+from src.customer_api.customers import customers_blueprint
+from src.employee_api.employees import employees_blueprint
+from src.manufacturer_api.manufacturers import manufacturers_blueprint
 
 # create the app object
 app = create_app()
+
+# add db config variables to the app object
+app.config['MYSQL_DATABASE_HOST'] = 'db'
+app.config['MYSQL_DATABASE_PORT'] = 3306
+app.config['MYSQL_DATABASE_USER'] = 'CEO'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'abc123'
+app.config['MYSQL_DATABASE_DB'] = ' FairWay'
+
+# register
+app.register_blueprint(customers_blueprint, url_prefix='/cust')
+app.register_blueprint(managers_blueprint, url_prefix='/mgr')
 
 if __name__ == '__main__':
     # we want to run in debug mode (for hot reloading) 
