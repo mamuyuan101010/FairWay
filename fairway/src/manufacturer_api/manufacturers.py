2bc2af70695c80bb44d096149af27969b21dc269
mamuyuan101010 <114116666+mamuyuan101010@users.noreply.github.com>
2022-12-13 11:43:27 -0500

    leo's commit

--- fairway/src/manufacturer_api/manufacturers.py
+++ fairway/src/manufacturer_api/manufacturers.py
@@ -2,28 +2,30 @@ from flask import Blueprint, request, jsonify, make_response
 import json
 from src import db
 
-manufacturer = Blueprint('manufacturer_blueprint', __name__)
+manufacturers = Blueprint('manufacturers', __name__)
+
 
 # Add a manufacturer to the DB
-@manufacturer.route('/manufacturer/add', methods=['POST'])
+@manufacturers.route('/manufacturers/add', methods=['POST'])
 def add_manufacturer():
+    current_app.logger.info(request.form)
     cursor = db.get_db().cursor()
-    manufacturerID = request.form['manufacturerID'] 
-    phoneNumber = request.form['phoneNumber'] 
+    country = request.form['country']
+    state = request.form['state']
     email = request.form['email'] 
-    firstName = request.form['firstName'] 
-    lastName = request.form['lastName']
-    query = f'INSERT INTO manufacturer(manufacturerID, phoneNumber, email, fname, lname) VALUES(\"{manufacturerID}", \"{phoneNumber}", \"{email}", \"{firstName}", \"{lastName}" )' 
+    phoneNumber = request.form['phoneNumber'] 
+    firstname = request.form['firstName'] 
+    lastname = request.form['lastName']
+    query = f'INSERT INTO Manufacturer(Country, State_Or_Territory, Email, PhoneNum, First_Name, Last_Name) VALUES(\"{country}\", \"{state}\", \"{email}\", \"{phoneNumber}\", \"{firstname}\", \"{lastname}\")' 
     cursor.execute(query)
     db.get_db().commit()
     return "Success!"
 
-# Get all manufacturer from the DB
-@manufacturer.route('/manufacturer', methods=['GET'])
-def get_manufacturer():
+# Get all manufacturers from the DB
+@manufacturers.route('/manufacturers', methods=['GET'])
+def get_manufacturers():
     cursor = db.get_db().cursor()
-    cursor.execute('select manufacturerID, phoneNumber, email, fname, \
-        lname from manufacturer')
+    cursor.execute('select ManufacturerID, PhoneNum, Email, First_Name, Last_Name from Manufacturer')
     row_headers = [x[0] for x in cursor.description]
     json_data = []
     theData = cursor.fetchall()
@@ -34,11 +36,11 @@ def get_manufacturer():
     the_response.mimetype = 'application/json'
     return the_response
 
-# Get manufacturer detail for manufacturer with particular manufacturerID
-@manufacturer.route('/manufacturer/<manufacturerID>', methods=['GET'])
-def get_manufacturer(manufacturerID):
+# Get manufacturer detail for manufacturer with particular userID
+@manufacturers.route('/manufacturers/<userID>', methods=['GET'])
+def get_manufacturer(userID):
     cursor = db.get_db().cursor()
-    cursor.execute('select * from manufacturer where manufacturerID = {0}'.format(manufacturerID))
+    cursor.execute('select * from Manufacturer where ManufacturerID = {0}'.format(userID))
     row_headers = [x[0] for x in cursor.description]
     json_data = []
     theData = cursor.fetchall()
